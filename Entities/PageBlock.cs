26d1b4674540ae8fa62610d5d7fcb2aba123f26b
Ilya <il.gareni4@gmail.com>
2022-03-12 19:03:06 +0300

    Added the ability to set page cover and change its position

--- Entities/PageBlock.cs
+++ Entities/PageBlock.cs
@@ -2,7 +2,46 @@
 {
     public class PageBlock : IconContentBlock
     {
+        public string? Cover { get; set; }
+        public float? CoverPosition { get; set; }
+
         public PageBlock(Api.Response.RecordMapBlockValue blockValue) : base(blockValue)
-        { }
+        {
+            Cover = blockValue?.Format?.PageCover;
+            CoverPosition = blockValue?.Format?.PageCoverPosition;
+        }
+
+        public async Task SetCover(string? pageCover)
+        {
+            if (pageCover == Cover) return;
+            try
+            {
+                Dictionary<string, object?> args = new() { { "page_cover", pageCover } };
+                Api.Request.Operation operation = Api.OperationBuilder.MainOperation(Api.MainCommand.update, Id, "block", new string[] { "format" }, args);
+                (await QuickRequestSetup.SaveTransactions(operation).Send(Client.HttpClient)).EnsureSuccessStatusCode();
+                Cover = pageCover;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task ChangeCoverPosition(float coverPosition)
+        {
+            if (coverPosition < 0 || coverPosition > 1) throw new ArgumentException("The cover position must be between 0 and 1 inclusive", "coverPosition");
+            if (coverPosition == CoverPosition) return;
+            try
+            {
+                Dictionary<string, object?> args = new() { { "page_cover_position", coverPosition } };
+                Api.Request.Operation operation = Api.OperationBuilder.MainOperation(Api.MainCommand.update, Id, "block", new string[] { "format" }, args);
+                (await QuickRequestSetup.SaveTransactions(operation).Send(Client.HttpClient)).EnsureSuccessStatusCode();
+                CoverPosition = coverPosition;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
     }
 }
